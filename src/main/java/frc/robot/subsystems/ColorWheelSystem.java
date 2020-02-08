13d87927bab844ed2fe24192abef6d1fc8497b56
Jack Troop <jwmtroop@gmail.com>
2020-02-08 14:00:27 -0500

    Color wheel 2 electric boogaloo

--- src/main/java/frc/robot/subsystems/ColorWheelSystem.java
+++ src/main/java/frc/robot/subsystems/ColorWheelSystem.java
@@ -7,6 +7,10 @@
 
 package frc.robot.subsystems;
 
+import javax.lang.model.util.ElementScanner6;
+
+import com.ctre.phoenix.motorcontrol.ControlMode;
+import com.ctre.phoenix.motorcontrol.can.VictorSPX;
 import com.revrobotics.ColorSensorV3;
 
 import edu.wpi.first.wpilibj.I2C;
@@ -14,7 +18,9 @@ import edu.wpi.first.wpilibj.VictorSP;
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import edu.wpi.first.wpilibj.util.Color;
 import edu.wpi.first.wpilibj.util.Color8Bit;
+import edu.wpi.first.wpilibj2.command.PrintCommand;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
+import jdk.jfr.Percentage;
 
 public class ColorWheelSystem extends SubsystemBase {
   /**
@@ -24,16 +30,16 @@ public class ColorWheelSystem extends SubsystemBase {
   ColorSensorV3 colorSensor;
   Color currentColor;
 
-  Color8Bit YELLOW = new Color8Bit((int)(.32 *255), (int)(.13 *255), (int)(.56*255));
-  Color8Bit RED = new Color8Bit((int)(.42*255), (int)(.4*255), (int)(.18*255));
-  Color8Bit GREEN = new Color8Bit((int)(.2*255), (int)(.54*255), (int)(.26*255));
-  Color8Bit BLUE = new Color8Bit((int)(.15*255), (int)(.44*255), (int)(.4*255));
+  public Color8Bit YELLOW = new Color8Bit((int)(.32 *255), (int)(.13 *255), (int)(.56*255));
+  public Color8Bit RED = new Color8Bit((int)(.42*255), (int)(.4*255), (int)(.18*255));
+ public Color8Bit GREEN = new Color8Bit((int)(.2*255), (int)(.54*255), (int)(.26*255));
+  public Color8Bit BLUE = new Color8Bit((int)(.15*255), (int)(.44*255), (int)(.4*255));
 
-  VictorSP colorWheel;
+ private VictorSPX colorWheel;
   double VARY = .1;
 
 
-  public ColorWheelSystem(VictorSP cw) {
+  public ColorWheelSystem(VictorSPX cw) {
 i2cPort = I2C.Port.kOnboard;
 colorSensor = new ColorSensorV3(i2cPort);
 
@@ -42,7 +48,13 @@ currentColor = colorSensor.getColor();
 colorWheel = cw;
  }
 
- boolean isColor(Color8Bit desiredColor) {
+
+public void setColorWheel(double speed){
+colorWheel.set(ControlMode.PercentOutput, .5);
+}
+
+
+ public boolean isColor(Color8Bit desiredColor) {
   double r = currentColor.red;
   double g = currentColor.green;
   double b = currentColor.blue;
@@ -57,19 +69,40 @@ colorWheel = cw;
   }
 }
 
-public void showColor() {
+
+public int showColor() {
   currentColor = colorSensor.getColor();
   // System.out.println(currentColor.red*255*2 + ", " + currentColor.green*255*2 +
   // ", " + currentColor.blue*2*255);
+
+// Red = 1
+// Yellow = 2
+// Blue = 3
+// Green = 4
   if (isColor(BLUE))
+  {
       System.out.println("Blue");
-  ;
-  if (isColor(RED))
+      return 3;
+  }
+  else if (isColor(RED))
+  {
       System.out.println("Red");
-  if (isColor(GREEN))
+      return 1;
+  }
+  else if (isColor(GREEN))
+  {
       System.out.println("Green");
-  if (isColor(YELLOW))
+      return 4;
+  }
+  else if (isColor(YELLOW))
+  {
       System.out.println("Yellow");
+      return 2;
+  }
+  else{
+    System.out.println("Failed");
+return 5;
+  }
 }
 
 public void dispColor() {
